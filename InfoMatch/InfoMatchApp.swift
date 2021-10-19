257b188b2cee186c5a7ba81b029590e84932e075
Fernando Lobo <fernandosclb@gmail.com>
2021-10-19 11:04:16 -0300

    First commit

---
+++ InfoMatch/InfoMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoMatchApp.swift
+//  InfoMatch
+//
+//  Created by Fernando Lobo on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct InfoMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
